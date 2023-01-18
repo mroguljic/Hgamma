a882427278635478ed9cc1aa9f741d0b398dd843
Matej Roguljic <matej.roguljic@gmail.com>
2023-01-18 16:36:17 +0100

    condor stuff

---
+++ condor/paths.py
@@ -0,0 +1,16 @@
+#Location of the main Hgamma directory
+HGAMMA_DIR          = "/users/mrogul/Work/Hgamma/Hgamma"
+#Where to store selection trees
+SELECTION_DIR       = HGAMMA_DIR+"/results/selection/"
+#Where to store skimmed NanoAODs
+SKIM_DIR            = "/STORE/matej/Hgamma_skims/v9/"
+#Where to store condor logs for selection jobs
+SELECTION_JOB_DIR   = HGAMMA_DIR+"/condor/selection_jobs"
+#Where to store condor logs for skimming jobs
+SKIM_JOB_DIR        = HGAMMA_DIR+"/condor/skim_jobs"
+#Location of the directory where timber-env in located (not the location of timber-env itself!)
+TIMBERENV_DIR       = "/users/mrogul/Work/Hgamma/"
+#Location of the CMSSW
+CMSSW_DIR           = "/users/mrogul/Work/Hgamma/CMSSW_12_3_0/"
+#Where to store condor logs for template jobs
+TEMPLATE_JOB_DIR    = HGAMMA_DIR+"/condor/template_jobs/"
---
+++ condor/templates.py
@@ -0,0 +1,78 @@
+from paths import CMSSW_DIR, TIMBERENV_DIR, HGAMMA_DIR
+
+
+selection_condor = """universe              = vanilla
+executable            = EXEC
+output                = OUTPUT/output_$(Process).out
+error                 = OUTPUT/output_$(Process).err
+log                   = OUTPUT/output_$(Process).log
+Arguments = "$(args)"
+use_x509userproxy = true
+Queue args from ARGFILE
+queue
+"""
+
+selection_template='''#!/bin/bash
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+cd {0}
+eval `scramv1 runtime -sh`
+cd {1}
+source timber-env/bin/activate
+
+export WORK_DIR={2}
+cd JOB_DIR
+
+echo eventSelection.py $*
+python $WORK_DIR/eventSelection.py $* --var nom
+python $WORK_DIR/eventSelection.py $* --var jesDown
+python $WORK_DIR/eventSelection.py $* --var jesUp
+python $WORK_DIR/eventSelection.py $* --var jerDown
+python $WORK_DIR/eventSelection.py $* --var jerUp
+python $WORK_DIR/eventSelection.py $* --var jmsUp
+python $WORK_DIR/eventSelection.py $* --var jmsDown
+python $WORK_DIR/eventSelection.py $* --var jmrUp
+python $WORK_DIR/eventSelection.py $* --var jmrDown
+'''.format(CMSSW_DIR,TIMBERENV_DIR,HGAMMA_DIR)
+
+selection_template_data='''#!/bin/bash
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+cd {0}
+eval `scramv1 runtime -sh`
+cd {1}
+source timber-env/bin/activate
+
+export WORK_DIR={2}
+cd JOB_DIR
+
+echo eventSelection.py $*
+python $WORK_DIR/eventSelection.py $* --var nom
+'''.format(CMSSW_DIR,TIMBERENV_DIR,HGAMMA_DIR)
+
+skim_template='''#!/bin/bash
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+cd {0}
+eval `scramv1 runtime -sh`
+cd {1}
+source timber-env/bin/activate
+
+export WORK_DIR={2}
+cd JOB_DIR
+
+echo $WORK_DIR/snapshot.py $*
+python $WORK_DIR/snapshot.py $*
+'''.format(CMSSW_DIR,TIMBERENV_DIR,HGAMMA_DIR)
+
+#Condor exe template for running jobs which create histogram templates :)
+templates_template='''#!/bin/bash
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+cd {0}
+eval `scramv1 runtime -sh`
+cd {1}
+source timber-env/bin/activate
+
+export WORK_DIR={2}
+cd $WORK_DIR
+
+echo $WORK_DIR/templateCreation.py $*
+python $WORK_DIR/templateCreation.py $*
+'''.format(CMSSW_DIR,TIMBERENV_DIR,HGAMMA_DIR)
